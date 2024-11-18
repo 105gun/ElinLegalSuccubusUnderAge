316d195a20f9966c1d4bdde4f1242be372d65544
105gun <ooo693428@163.com>
2024-11-19 03:14:21 +0800

    1.0.0

---
+++ Plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LegalSuccubusMod;
+
+[BepInPlugin("105gun.legalsuccubus.mod", "Legal Succubus Underage", "1.0.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    public static bool inSuccubusVisitCheck = false;
+    private void Start()
+    {
+        var harmony = new Harmony("105gun.legalsuccubus.mod");
+        harmony.PatchAll();
+        System.Console.WriteLine("[LegalSuccubusMod] Initialization completed.");
+    }
+}
+
+// ConSleep.SuccubusVisit
+[HarmonyPatch(typeof(ConSleep), "SuccubusVisit")]
+public class SuccubusVisitPatch
+{
+    static void Prefix()
+    {
+        Plugin.inSuccubusVisitCheck = true;
+    }
+
+    static void Postfix()
+    {
+        Plugin.inSuccubusVisitCheck = false;
+    }
+}
+
+// Biography.IsUnderAge get
+[HarmonyPatch(typeof(Biography), "IsUnderAge", MethodType.Getter)]
+public class IsUnderAgePatch
+{
+    static void Postfix(ref bool __result)
+    {
+        if (Plugin.inSuccubusVisitCheck)
+        {
+            __result = false;
+            Plugin.inSuccubusVisitCheck = false; // Just in case some strange things happen
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Legal Succubus Underage
+
+Template from https://github.com/MinusGix/ElinExampleMod
+Run "dotnet build" to build
